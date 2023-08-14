2058d8a51677f0482dd76a735e654f606c87c91d
VIJAY <vijaydhiman51@gmail.com>
2023-08-15 01:43:11 +0530

    Initial Commit - Functional Components

---
+++ src/components/card-container/card-container.component.jsx
@@ -0,0 +1,18 @@
+import "./card-container.styles.css";
+
+//const CardConatiner = ({ monster: { id, name, email } }) => (
+    
+const CardConatiner = ({ monster }) => {
+    const { id, name, email } = monster;
+    return (
+        <div className='card-container' key={id}>
+            <img
+                alt={`monster ${name}`}
+                src={`https://robohash.org/${id}?set=set2&size=180x180`} />
+            <h2>{name}</h2>
+            <p>{email}</p>
+        </div>
+    )
+}
+
+export default CardConatiner;
